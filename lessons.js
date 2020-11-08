85d1fffcb67a01b17beb9f943b5cf18f560dacb3
Mithun <mithain3@icloud.com>
2020-11-08 12:25:04 +0400

    Setup lessons.js

---
+++ lessons.js
@@ -0,0 +1,82 @@
+let lessons = [
+    {
+        id: 1,
+        subject: 'Maths',
+        location: 'London Campus',
+        price: 1001,
+        space: 5,
+        icon: "fas fa-square-root-alt"
+    },
+    {
+        id: 2,
+        subject: 'Science',
+        location: 'Dubai Campus',
+        price: 1002,
+        space: 5,
+        icon: "fas fa-atom"
+    },
+    {
+        id: 3,
+        subject: 'Geography',
+        location: 'Malta Campus',
+        price: 1003,
+        space: 5,
+        icon: "fas fa-globe-americas"
+    },
+    {
+        id: 4,
+        subject: 'Geography',
+        location: 'Malta Campus',
+        price: 1004,
+        space: 5,
+        icon: "fas fa-globe-americas"
+    },
+    {
+        id: 5,
+        subject: 'Business',
+        location: 'London Campus',
+        price: 1005,
+        space: 5,
+        icon: "fas fa-briefcase"
+    },
+    {
+        id: 6,
+        subject: 'Law',
+        location: 'Malta Campus',
+        price: 1006,
+        space: 5,
+        icon: "fas fa-landmark"
+    },
+    {
+        id: 7,
+        subject: 'Engineering',
+        location: 'Dubai Campus',
+        price: 1007,
+        space: 5,
+        icon: "fas fa-laptop-code"
+    },
+    {
+        id: 8,
+        subject: 'Accounting',
+        location: 'Dubai Campus',
+        price: 1008,
+        space: 5,
+        icon: "fas fa-balance-scale-right"
+    },
+    {
+        id: 9,
+        subject: 'Architecture',
+        location: 'London Campus',
+        price: 1009,
+        space: 5,
+        icon: "fas fa-torii-gate"
+    },
+    {
+        id: 10,
+        subject: 'Medicine',
+        location: 'Malta Campus',
+        price: 1010,
+        space: 5,
+        icon: "fas fa-stethoscope"
+    },
+]
