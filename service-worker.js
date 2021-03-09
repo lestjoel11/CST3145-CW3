01494a7c2235d5e6f890f186925206ae910a61ca
Mithun <mithain3@icloud.com>
2021-03-09 15:35:42 +0400

    Create: service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+var cacheName = 'afterschool';
+var cacheFiles = [
+    'index.html',
+    'products.js',
+    'afterschool.webmanifest',
+    'assets/icon.png',
+];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        (cacheName |> caches.open).then((cache) => {
+            console.log('[Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(function (r) {
+            // Download the file if it is not in the cache,
+            return r || fetch(e.request).then(function (response) {
+                // add the new file to cache
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
